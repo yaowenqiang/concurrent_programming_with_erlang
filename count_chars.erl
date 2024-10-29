6bf24564fe166dd05fd7895a759b0c2fde604b16
姚文强 <yaowenqiang111@163.com>
2024-10-30 00:18:35 +0800

    bug fix

--- count_chars.erl
+++ count_chars.erl
@@ -3,10 +3,15 @@
 count_characters(Str) ->
   count_characters(Str, #{}).
 
-count_characters([H|T], #{ H => N} = X) -> 
-  count_characters(T, X#{H := N + 1});
+%count_characters([H|T], #{ H := N} = X) -> 
+%  count_characters(T, X#{H := N + 1});
+%count_characters([H|T], X) -> 
+%  count_characters(T, X#{H => 1});
 count_characters([H|T], X) -> 
-  count_characters(T, X#{H => 1});
+  case maps:get(H, X, undefined) of
+    undefined -> count_characters(T, X#{H => 1});
+    N -> count_characters(T, X#{H := N + 1})
+  end;
 count_characters([], X) -> 
   X.
 
