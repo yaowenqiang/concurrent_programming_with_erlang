c9289d751441a240c5b759e9bcd82c8f76ef3c45
姚文强 <yaowenqiang111@163.com>
2024-10-29 23:45:19 +0800

    add count_chars demo

---
+++ count_chars.erl
@@ -0,0 +1,13 @@
+-module(count_chars).
+-export([count_characters/1]).
+count_characters(Str) ->
+  count_characters(Str, #{}).
+
+count_characters([H|T], #{ H => N} = X) -> 
+  count_characters(T, X#{H := N + 1});
+count_characters([H|T], X) -> 
+  count_characters(T, X#{H => 1});
+count_characters([], X) -> 
+  X.
+
+
